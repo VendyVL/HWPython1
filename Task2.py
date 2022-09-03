b99f6ff4298af317e31718fbc84fd82bd6ed2acb
«VendyVL» <vedmansk@yandex.ru>
2022-09-03 22:17:03 +0300

    обновлено, вторая улучшена

--- Task2.py
+++ Task2.py
@@ -1,11 +1,25 @@
 #Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-# Что это за абракадабра?
+
 
 print(f'x | y | z | ¬x | ¬y | ¬z | X ⋁ Y ⋁ Z | ¬(X ⋁ Y ⋁ Z) | ¬X ⋀ ¬Y ⋀ ¬Z')
 
 for x in range(0,2):
     for y in range(0,2):
         for z in range(0,2):
-            print(f'{x} | {y} | {z} | {~x} | {~y} | {~z} |     {x|y|z}     |     {~(x|y|z)}       |   {~x&~y&~z}')
+            if x == 0: a = 1
+            else: a = 0
+            if y == 0: b = 1
+            else: b = 0
+            if z == 0: c = 1
+            else: c = 0
 
-# Где оно берёт -2 вот в чём вопрос.
+            if x == 1 or y == 1 or z == 1: d = 1
+            else: d = 0
+
+            if d == 0: e = 1
+            else: e = 0
+
+            if a == 0 or b == 0 or c == 0: f = 0
+            else: f = 1
+            print(f'{x} | {y} | {z} | {a}  | {b}  | {c}  |      {d}    |       {e}      |    {f}')
+
